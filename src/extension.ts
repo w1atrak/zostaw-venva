acebfc8712ec1b89cddf4cac5de5bb87a9ac63fa
w1atrak <danielbielaszka@gmail.com>
2025-10-29 21:52:30 +0100

    initial version

---
+++ src/extension.ts
@@ -0,0 +1,57 @@
+import * as vscode from "vscode";
+
+let debounceTimer: NodeJS.Timeout | undefined;
+let substrings: string[];
+let warningMessage: string;
+
+function loadConfig() {
+    substrings = vscode.workspace
+        .getConfiguration("zostaw-venva")
+        .get("venvPaths", []);
+    warningMessage = vscode.workspace
+        .getConfiguration("zostaw-venva")
+        .get("warningMessage", "💀");
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    loadConfig();
+
+    context.subscriptions.push(
+        vscode.workspace.onDidChangeTextDocument(event => {
+            if (
+                event.document === vscode.window.activeTextEditor?.document &&
+                event.contentChanges.length > 0
+            ) {
+                if (debounceTimer) {
+                    clearTimeout(debounceTimer);
+                }
+
+                debounceTimer = setTimeout(() => {
+                    const fileName: string =
+                        event.document.fileName.toLowerCase();
+
+                    if (substrings.some(s => fileName.includes(s))) {
+                        vscode.window.showWarningMessage(warningMessage);
+                    }
+                }, 1000);
+            }
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.workspace.onDidChangeConfiguration(event => {
+            if (
+                event.affectsConfiguration("zostaw-venva.venvPaths") ||
+                event.affectsConfiguration("zostaw-venva.warningMessage")
+            ) {
+                loadConfig();
+            }
+        })
+    );
+}
+
+export function deactivate() {
+    if (debounceTimer) {
+        clearTimeout(debounceTimer);
+    }
+}
